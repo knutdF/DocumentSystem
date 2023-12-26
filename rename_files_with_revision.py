c5c3aa17410461a5c7fa9c5f004d14699cb53e69
knutdF <45771757+knutdF@users.noreply.github.com>
2023-12-26 22:51:09 +0100

    Update rename_files_with_revision.py

--- rename_files_with_revision.py
+++ rename_files_with_revision.py
@@ -3,17 +3,15 @@ import subprocess
 
 def get_last_commit_id(file_path):
     try:
-        # Ermitteln der letzten Commit-ID für die Datei
         commit_id = subprocess.check_output(['git', 'log', '-n', '1', '--pretty=format:%h', '--', file_path]).decode().strip()
         return commit_id
     except subprocess.CalledProcessError:
         return None
 
-# Durchlaufen aller Dateien im aktuellen Verzeichnis (und Unterordnern)
 for root, dirs, files in os.walk('.'):
     for name in files:
-        if name.startswith('.'):
-            continue  # Ignoriere versteckte Dateien und Ordner
+        if name.startswith('.') or root.startswith('./.github/workflows'):
+            continue
         file_path = os.path.join(root, name)
         commit_id = get_last_commit_id(file_path)
         if commit_id:
@@ -21,6 +19,5 @@ for root, dirs, files in os.walk('.'):
             new_file_name = f"{file_name}-{commit_id}{file_extension}"
             new_file_path = os.path.join(root, new_file_name)
             
-            # Umbenennen der Datei
             print(f"Umbenennen: {file_path} zu {new_file_path}")
             os.rename(file_path, new_file_path)
