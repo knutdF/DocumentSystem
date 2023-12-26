a2c235afb2ff93c3009cb8567265b3aa79d4d977
knutdF <45771757+knutdF@users.noreply.github.com>
2023-12-26 22:24:12 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,24 +1,26 @@
 import os
+import subprocess
 
-# Überprüfen, ob die VERSION-Datei existiert
-if not os.path.isfile('VERSION'):
-    print("VERSION file not found")
-    exit(1)
-
-# Lesen der Version aus der VERSION-Datei
-with open('VERSION', 'r') as file:
-    version = file.read().strip()
+def get_last_commit_id(file_path):
+    try:
+        # Ermitteln der letzten Commit-ID für die Datei
+        commit_id = subprocess.check_output(['git', 'log', '-n', '1', '--pretty=format:%h', '--', file_path]).decode().strip()
+        return commit_id
+    except subprocess.CalledProcessError:
+        return None
 
 # Durchlaufen aller Dateien im aktuellen Verzeichnis (und Unterordnern)
 for root, dirs, files in os.walk('.'):
     for name in files:
         if name.startswith('.'):
             continue  # Ignoriere versteckte Dateien und Ordner
-        old_file_path = os.path.join(root, name)
-        file_name, file_extension = os.path.splitext(name)
-        new_file_name = f"{file_name}-{version}{file_extension}"
-        new_file_path = os.path.join(root, new_file_name)
-        
-        # Umbenennen der Datei
-        print(f"Umbenennen: {old_file_path} zu {new_file_path}")
-        os.rename(old_file_path, new_file_path)
+        file_path = os.path.join(root, name)
+        commit_id = get_last_commit_id(file_path)
+        if commit_id:
+            file_name, file_extension = os.path.splitext(name)
+            new_file_name = f"{file_name}-{commit_id}{file_extension}"
+            new_file_path = os.path.join(root, new_file_name)
+            
+            # Umbenennen der Datei
+            print(f"Umbenennen: {file_path} zu {new_file_path}")
+            os.rename(file_path, new_file_path)
